00b2195e5709f6a15f61be4cb8a173fc99ed7078
Tri <abeces968@gmail.com>
2023-02-18 03:10:49 +0700

    Scaffold calculation history

---
+++ app/src/main/java/com/tri/calculator/CalcHistory.java
@@ -0,0 +1,14 @@
+package com.tri.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalcHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_history);
+    }
+}
